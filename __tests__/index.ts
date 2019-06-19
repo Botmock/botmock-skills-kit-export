144533506119b3733f5c802b91fedd692042dfd1
Kevin Donahue <nonnontrivial@gmail.com>
2019-06-19 08:42:22 -0400

    add test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,16 +1,21 @@
 import { execSync } from "child_process";
 import { join } from "path";
 import fs from "fs";
-// import * as lib from "../lib";
+import * as lib from "../lib";
 
 test("can read from output directory after `npm start`", () => {
   const stdout = execSync("npm start");
   expect.assertions(2);
-  expect(Buffer.from(stdout).length).toBe(160);
+  expect(Buffer.from(stdout).length).toBe(147);
   expect(async () => {
     await fs.promises.access(join(process.cwd(), "output"));
   }).not.toThrow();
 });
 
-test.todo("produces interaction model");
+test("produces interaction model from project data", () => {
+  expect(() => {
+    lib.mapProjectDataToInteractionModel([]);
+  }).toThrow();
+});
+
 test.todo("creates directory named from provided command line flag");
