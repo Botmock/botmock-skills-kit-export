66d11cf672e4ae558dab93c1ef2af21b4e57e8d7
Kevin Donahue <nonnontrivial@gmail.com>
2020-02-13 11:27:33 -0500

    export types

--- lib/index.ts
+++ lib/index.ts
@@ -1,37 +1,12 @@
 import uuid from "uuid/v4";
 import { symmetricWrap } from "@botmock-api/utils";
 import { DEFAULT_INTENTS } from "../templates";
-// import { Botmock } from "@botmock-api/client";
-
-enum DelegationStrategies {
-  ALWAYS = "ALWAYS",
-  SKILL_RESPONSE = "SKILL_RESPONSE",
-}
-
-type ObjectLike<T> = { [resourceName: string]: T; };
-
-export namespace SkillsKit {
-  export type InteractionModel = {
-    languageModel: {
-      invocationName: string;
-      intents?: any[];
-      types?: { values: { name: { value: string; synonyms: string[]; }; }[]; }[];
-    };
-    dialog?: {
-      delegationStrategy?: string;
-      intents: any[];
-    };
-    prompts?: any;
-  };
-}
-
-export namespace Botmock {
-  export type Slot = {
-    samples: string[];
-  };
-  export type Intent = any;
-  export type Utterance = any;
-}
+import {
+  SkillsKit,
+  Botmock,
+  ObjectLike,
+  DelegationStrategies,
+} from "./types";
 
 /**
  * @param data public API response payload
---
+++ lib/types/index.ts
@@ -0,0 +1,29 @@
+export enum DelegationStrategies {
+  ALWAYS = "ALWAYS",
+  SKILL_RESPONSE = "SKILL_RESPONSE",
+}
+
+export type ObjectLike<T> = { [resourceName: string]: T; };
+
+export namespace SkillsKit {
+  export type InteractionModel = {
+    languageModel: {
+      invocationName: string;
+      intents?: any[];
+      types?: { values: { name: { value: string; synonyms: string[]; }; }[]; }[];
+    };
+    dialog?: {
+      delegationStrategy?: string;
+      intents: any[];
+    };
+    prompts?: any;
+  };
+}
+
+export namespace Botmock {
+  export type Slot = {
+    samples: string[];
+  };
+  export type Intent = any;
+  export type Utterance = any;
+}
